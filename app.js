4f8cd265500190290eaf9ae29994c35894fdc650
grace-nm <grace.mcloughlin@yahoo.co.nz>
2021-10-21 19:31:56 +1300

    showing how i made tables and data base chroniclogly/ in time

--- app.js
+++ app.js
@@ -15,105 +15,6 @@ function getConn(){
         port: 3306
     });
 }
-// Creating table for  level four
-// getConn().connect((err)=>{
-//     if(err) throw err
-//     console.log('Connected');
-//     //create table:
-//     let db = `CREATE TABLE markingRubricsLevelFour (id INT AUTO_INCREMENT PRIMARY KEY, student1name VARCHAR(50), student1comment1 VARCHAR(300), student1comment2 VARCHAR(300), student1comment3 VARCHAR(300), student1comment4 VARCHAR(300), student1comment5 VARCHAR(300), student2name VARCHAR(50), student2comment1 VARCHAR(300), student2comment2 VARCHAR(300), student2comment3 VARCHAR(300), student2comment4 VARCHAR(300), student2comment5 VARCHAR(300), student3name VARCHAR(50), student3comment1 VARCHAR(300), student3comment2 VARCHAR(300), student3comment3 VARCHAR(300), student3comment4 VARCHAR(300), student3comment5 VARCHAR(300), student4name VARCHAR(50), student4comment1 VARCHAR(300), student4comment2 VARCHAR(300), student4comment3 VARCHAR(300), student4comment4 VARCHAR(300), student4comment5 VARCHAR(300), student5name VARCHAR(50), student5comment1 VARCHAR(300), student5comment2 VARCHAR(300), student5comment3 VARCHAR(300), student5comment4 VARCHAR(300), student5comment5 VARCHAR(300), student6name VARCHAR(50), student6comment1 VARCHAR(300), student6comment2 VARCHAR(300), student6comment3 VARCHAR(300), student6comment4 VARCHAR(300), student6comment5 VARCHAR(300))`;
-//     getConn().query(db,(err, res)=>{
-//         if(err) throw err;
-//         console.log('TABLE Created')
-//     })
-// })
-
-// Creating table for  level five
-// getConn().connect((err)=>{
-//     if(err) throw err
-//     console.log('Connected');
-//     //create table:
-//         let db = 'CREATE TABLE markingRubricsLevelFive (id INT AUTO_INCREMENT PRIMARY KEY, ';
-//             db += 'studentName VARCHAR(50), ';
-//             db += 'comment1 VARCHAR(250), comment2 VARCHAR(250), comment3 VARCHAR(250), ';
-//             db += 'criteria1 BOOLEAN, criteria2 BOOLEAN, criteria3 BOOLEAN, criteria4 BOOLEAN, criteria5 BOOLEAN, criteria6 BOOLEAN)'
-
-//      getConn().query(db,(err, res)=>{
-//         if(err) throw err;
-//         console.log('TABLE Created')
-//     })
-// })
-
-// Creating table for  level four
-// getConn().connect((err)=>{
-//     if(err) throw err
-//     console.log('Connected');
-//     //create table:
-//         let db = 'CREATE TABLE markingRubricsLevelFour (id INT AUTO_INCREMENT PRIMARY KEY, ';
-//             db += 'studentName VARCHAR(50), ';
-//             db += 'comment1 VARCHAR(250), comment2 VARCHAR(250), comment3 VARCHAR(250), comment4 VARCHAR(250), comment5 VARCHAR(250), ';
-//             db += 'criteria1 BOOLEAN, criteria2 BOOLEAN, criteria3 BOOLEAN, criteria4 BOOLEAN, criteria5 BOOLEAN, criteria6 BOOLEAN, criteria7 BOOLEAN, criteria8 BOOLEAN, criteria9 BOOLEAN, criteria10 BOOLEAN, criteria11 BOOLEAN, criteria12 BOOLEAN, criteria13 BOOLEAN, criteria14 BOOLEAN)'
-
-//      getConn().query(db,(err, res)=>{
-//         if(err) throw err;
-//         console.log('TABLE Created')
-//     })
-// })
-
-const router = require('./routes/midCourse');
-const postCourseRouter = require('./routes/postCourse');
-const peerReviewLevelFour = require('./routes/peerReviewLevelFour');
-const selfReflectionLevelFour = require('./routes/selfReflectionLevelFour');
-const missionxEvaluationLevelFour= require('./routes/missionxEvaluation');
-const individualAssessorEvaluationLevelFour= require('./routes/individualAssessorEvaluationLevelFour');
-const individualAssessorEvaluationLevelFive= require('./routes/individualAssessorEvaluationLevelFive');
-const peerReviewLevelFive = require('./routes/peerReviewLevelFive');
-const finalProjectEvaluationLevelFive = require('./routes/finalProjectEvaluationLevelFive');
-const markingRubricsLevelFive = require('./routes/markingRubricsLevelFive');
-const markingRubricsLevelFour = require('./routes/markingRubricsLevelFour'); //need to include in below statement!!
-
-//include marking rubrics ones
-app.use(router, postCourseRouter, peerReviewLevelFour, selfReflectionLevelFour, missionxEvaluationLevelFour, individualAssessorEvaluationLevelFour, individualAssessorEvaluationLevelFive, peerReviewLevelFive, finalProjectEvaluationLevelFive, markingRubricsLevelFive, markingRubricsLevelFour)
-
-app.get('/midcoursefeedbackform', (req,res)=>{
-    console.log('fetching all user');
-    const sql = "SELECT * FROM midcoursefeedbackform";
-    getConn().query(sql, (err, rows, results)=>{
-if(err){
-    console.log('failed to fetch all user data.');
-    res.sendStatus(500);
-    res.end();
-    return
-}
-console.log('I think we fetched user successfully')
-res.json(rows);
-    })
-})
-
-app.get('/midcoursefeedbackform/:id', (req, res)=>{
-    console.log('fetching user with id : ', req.params.id);
-    const sql = 'SELECT * FROM midcoursefeedbackform WHERE id = ?';
-    const value = req.params.id;
-    getConn().query(sql, [value], (err, rows, fields)=>{
-        if(err){
-            console.log('Failed to fetch user with user id : ', err);
-            res.sendStatus(500)
-            res.end()
-            return
-        }
-        console.log('I think we fetched user with id');
-        res.json(rows)
-    })
-})
-
-app.get('/',(req, res) => {
-    console.log('responding to root route')
-    res.send('Your nodeJS is successfully setup')
-})
-
-app.listen(3333, () =>{
-    console.log('server running port is 3333');
-})
-
 //Creating DB (dataBase):
 // getConn().connect((err)=>{
 //     if(err) throw err
@@ -273,3 +174,103 @@ app.listen(3333, () =>{
 //(id INT AUTO_INCREMENT PRIMARY KEY, student1name VARCHAR(50), student1comment1 VARCHAR(300), student1comment2 VARCHAR(300), student1comment3 VARCHAR(300), student2comment1 VARCHAR(300), student2comment2 VARCHAR(300), student2comment3 VARCHAR(300), student3comment1 VARCHAR(300), student3comment2 VARCHAR(300), student3comment3 VARCHAR(300), student4comment1 VARCHAR(300), student4comment2 VARCHAR(300), student4comment3 VARCHAR(300), student5comment1 VARCHAR(300), student5comment2 VARCHAR(300), student5comment3 VARCHAR(300), student6comment1 VARCHAR(300), student6comment2 VARCHAR(300), student6comment3 VARCHAR(300))`;
 
     // let db = `CREATE TABLE markingRubricsLevelFive (id INT AUTO_INCREMENT PRIMARY KEY, student1name VARCHAR(50), student1comment1 VARCHAR(300), student1comment2 VARCHAR(300), student1comment3 VARCHAR(300), student2name VARCHAR(50), student2comment1 VARCHAR(300), student2comment2 VARCHAR(300), student2comment3 VARCHAR(300), student3name VARCHAR(50), student3comment1 VARCHAR(300), student3comment2 VARCHAR(300), student3comment3 VARCHAR(300), student4name VARCHAR(50), student4comment1 VARCHAR(300), student4comment2 VARCHAR(300), student4comment3 VARCHAR(300), student5name VARCHAR(50), student5comment1 VARCHAR(300), student5comment2 VARCHAR(300), student5comment3 VARCHAR(300), student6name VARCHAR(50), student6comment1 VARCHAR(300), student6comment2 VARCHAR(300), student6comment3 VARCHAR(300))`;
+
+// Creating table for  level four
+// getConn().connect((err)=>{
+//     if(err) throw err
+//     console.log('Connected');
+//     //create table:
+//     let db = `CREATE TABLE markingRubricsLevelFour (id INT AUTO_INCREMENT PRIMARY KEY, student1name VARCHAR(50), student1comment1 VARCHAR(300), student1comment2 VARCHAR(300), student1comment3 VARCHAR(300), student1comment4 VARCHAR(300), student1comment5 VARCHAR(300), student2name VARCHAR(50), student2comment1 VARCHAR(300), student2comment2 VARCHAR(300), student2comment3 VARCHAR(300), student2comment4 VARCHAR(300), student2comment5 VARCHAR(300), student3name VARCHAR(50), student3comment1 VARCHAR(300), student3comment2 VARCHAR(300), student3comment3 VARCHAR(300), student3comment4 VARCHAR(300), student3comment5 VARCHAR(300), student4name VARCHAR(50), student4comment1 VARCHAR(300), student4comment2 VARCHAR(300), student4comment3 VARCHAR(300), student4comment4 VARCHAR(300), student4comment5 VARCHAR(300), student5name VARCHAR(50), student5comment1 VARCHAR(300), student5comment2 VARCHAR(300), student5comment3 VARCHAR(300), student5comment4 VARCHAR(300), student5comment5 VARCHAR(300), student6name VARCHAR(50), student6comment1 VARCHAR(300), student6comment2 VARCHAR(300), student6comment3 VARCHAR(300), student6comment4 VARCHAR(300), student6comment5 VARCHAR(300))`;
+//     getConn().query(db,(err, res)=>{
+//         if(err) throw err;
+//         console.log('TABLE Created')
+//     })
+// })
+
+// Creating table for  level five
+// getConn().connect((err)=>{
+//     if(err) throw err
+//     console.log('Connected');
+//     //create table:
+//         let db = 'CREATE TABLE markingRubricsLevelFive (id INT AUTO_INCREMENT PRIMARY KEY, ';
+//             db += 'studentName VARCHAR(50), ';
+//             db += 'comment1 VARCHAR(250), comment2 VARCHAR(250), comment3 VARCHAR(250), ';
+//             db += 'criteria1 BOOLEAN, criteria2 BOOLEAN, criteria3 BOOLEAN, criteria4 BOOLEAN, criteria5 BOOLEAN, criteria6 BOOLEAN)'
+
+//      getConn().query(db,(err, res)=>{
+//         if(err) throw err;
+//         console.log('TABLE Created')
+//     })
+// })
+
+// Creating table for  level four
+// getConn().connect((err)=>{
+//     if(err) throw err
+//     console.log('Connected');
+//     //create table:
+//         let db = 'CREATE TABLE markingRubricsLevelFour (id INT AUTO_INCREMENT PRIMARY KEY, ';
+//             db += 'studentName VARCHAR(50), ';
+//             db += 'comment1 VARCHAR(250), comment2 VARCHAR(250), comment3 VARCHAR(250), comment4 VARCHAR(250), comment5 VARCHAR(250), ';
+//             db += 'criteria1 BOOLEAN, criteria2 BOOLEAN, criteria3 BOOLEAN, criteria4 BOOLEAN, criteria5 BOOLEAN, criteria6 BOOLEAN, criteria7 BOOLEAN, criteria8 BOOLEAN, criteria9 BOOLEAN, criteria10 BOOLEAN, criteria11 BOOLEAN, criteria12 BOOLEAN, criteria13 BOOLEAN, criteria14 BOOLEAN)'
+
+//      getConn().query(db,(err, res)=>{
+//         if(err) throw err;
+//         console.log('TABLE Created')
+//     })
+// })
+
+const router = require('./routes/midCourse');
+const postCourseRouter = require('./routes/postCourse');
+const peerReviewLevelFour = require('./routes/peerReviewLevelFour');
+const selfReflectionLevelFour = require('./routes/selfReflectionLevelFour');
+const missionxEvaluationLevelFour= require('./routes/missionxEvaluation');
+const individualAssessorEvaluationLevelFour= require('./routes/individualAssessorEvaluationLevelFour');
+const individualAssessorEvaluationLevelFive= require('./routes/individualAssessorEvaluationLevelFive');
+const peerReviewLevelFive = require('./routes/peerReviewLevelFive');
+const finalProjectEvaluationLevelFive = require('./routes/finalProjectEvaluationLevelFive');
+const markingRubricsLevelFive = require('./routes/markingRubricsLevelFive');
+const markingRubricsLevelFour = require('./routes/markingRubricsLevelFour'); //need to include in below statement!!
+
+//include marking rubrics ones
+app.use(router, postCourseRouter, peerReviewLevelFour, selfReflectionLevelFour, missionxEvaluationLevelFour, individualAssessorEvaluationLevelFour, individualAssessorEvaluationLevelFive, peerReviewLevelFive, finalProjectEvaluationLevelFive, markingRubricsLevelFive, markingRubricsLevelFour)
+
+app.get('/midcoursefeedbackform', (req,res)=>{
+    console.log('fetching all user');
+    const sql = "SELECT * FROM midcoursefeedbackform";
+    getConn().query(sql, (err, rows, results)=>{
+if(err){
+    console.log('failed to fetch all user data.');
+    res.sendStatus(500);
+    res.end();
+    return
+}
+console.log('I think we fetched user successfully')
+res.json(rows);
+    })
+})
+
+app.get('/midcoursefeedbackform/:id', (req, res)=>{
+    console.log('fetching user with id : ', req.params.id);
+    const sql = 'SELECT * FROM midcoursefeedbackform WHERE id = ?';
+    const value = req.params.id;
+    getConn().query(sql, [value], (err, rows, fields)=>{
+        if(err){
+            console.log('Failed to fetch user with user id : ', err);
+            res.sendStatus(500)
+            res.end()
+            return
+        }
+        console.log('I think we fetched user with id');
+        res.json(rows)
+    })
+})
+
+app.get('/',(req, res) => {
+    console.log('responding to root route')
+    res.send('Your nodeJS is successfully setup')
+})
+
+app.listen(3333, () =>{
+    console.log('server running port is 3333');
+})
+
